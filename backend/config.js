05816eb3f996d719f4794b10431387dae206f0cf
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-02-24 15:21:48 +0545

    Mongoose installed,mongodb setup done and database running successfully

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://root:root@bookstore-mern-app.rqlpnny.mongodb.net/MERN-PROJECT-2024?retryWrites=true&w=majority&appName=bookstore-mern-app'
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -8,6 +9,14 @@ app.get('/', (request, response) => {
     return response.status(234).send('WELCOME TO MERN STACK TUTORIAL');
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to Database')
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
