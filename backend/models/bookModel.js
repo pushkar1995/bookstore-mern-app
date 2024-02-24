904385368e8a43de9b489c7a5938758e6ede3b0e
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-02-24 16:55:27 +0545

    Posted books to database and get all books from Database

--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -10,7 +10,7 @@ const bookSchema = mongoose.Schema(
             type: String,
             required: true,
         },
-        publishedYear: {
+        publishYear: {
             type: Number,
             required: true,
         }
