f48cc4a2743605ec2ebe44e3ab4427bbd65828e2
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-02-24 20:03:18 +0545

    CRUD functionality implemented in backend

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,68 @@ app.get('/books', async (request, response) => {
     }
 })
 
+// Route for Get One Book from Database by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+// Route for Update a Book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            })
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found'});
+        }
+
+        return response.status(200).send({ message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+// Route for Delete a book
+app.delete('/books/:id', async (request, response) => {
+    try{
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id)
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+
+        return response.status(200).send({ message: 'Book deleted successfully' });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+
+})
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
