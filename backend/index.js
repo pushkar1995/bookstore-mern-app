2212876d5f46783bf08cd636c78fc46883a5c4b4
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-02-24 14:53:05 +0545

    Creating 1st HTTP Route in backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('WELCOME TO MERN STACK TUTORIAL');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 });
